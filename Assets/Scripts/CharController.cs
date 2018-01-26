a81d84e668ee0a57c1b504a6700f20ecebd9c84e
Alexander Scheurer <mail@aspepex.net>
2018-01-26 22:35:07 +0100

    Basic Movement

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+    public float DistanceFromGround = 0.5f;
+    public float DropRate = 0.3f;
+    public float MovementSpeedMultiplyer = 0.3f;
+    private Vector2 _positionVector;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _positionVector = this.transform.position;
+	}
+	
+	void FixedUpdate()
+    {
+        _positionVector.y -= DropRate;
+        this.transform.position = _positionVector;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up);
+        if (hit.collider != null) {
+            float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            float heightError = DistanceFromGround - distance;
+            _positionVector.y += heightError;
+        }
+
+        _positionVector.x += Input.GetAxis("Horizontal") * MovementSpeedMultiplyer;
+
+        this.transform.position = _positionVector;
+    }
+}
