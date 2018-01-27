24380f33ec75fe13c77ee24d65550472254939e9
noppelmax <gitkraken@noppelmax.online>
2018-01-27 17:41:23 +0100

    max

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,10 +7,15 @@ public class GameController : MonoBehaviour
 
     public GameObject msgBoxPrefab;
 
+    private Dictionary<string, bool> occuredActions = new Dictionary<string, bool>();
+
+
     public void Action(string Action, GameObject GO)
     {
         //biggest f-ing switch monster ever
 
+        // Add Action to occuredActions
+        occuredActions.add(Action, true);
 
         switch (Action)
         {
@@ -21,11 +26,18 @@ public class GameController : MonoBehaviour
                 mBoxCtrl.Initialize("Tzzzzzz....");
                 break;
             }
+            case "zaehne_putzen":
+            {
+                GameObject gObj = Instantiate(msgBoxPrefab);
+                MsgBoxCtrl mBoxCtrl = gObj.GetComponent<MsgBoxCtrl>();
+                mBoxCtrl.Initialize("Immer 3 Minuten Zähne putzen. Wichtig!");
+                break;
+            }
             case "dose_holen":
             {
                 GameObject gObj = Instantiate(msgBoxPrefab);
                 MsgBoxCtrl mBoxCtrl = gObj.GetComponent<MsgBoxCtrl>();
-                mBoxCtrl.Initialize("Mhmmm Ravioli. Lecker.");
+                mBoxCtrl.Initialize("Mhmmm Ravioli. Lecker. Jetzt wird gekocht...");
                 break;
             }
             default:
