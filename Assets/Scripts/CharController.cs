5836c59e911450d6c63dac5a06c5c623c35d1ed1
Alexander Scheurer <mail@aspepex.net>
2018-01-27 14:44:02 +0100

    Ladders and Interactables

--- Assets/Scripts/CharController.cs
+++ Assets/Scripts/CharController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharController : MonoBehaviour {
+public class CharController : MonoBehaviour
+{
 
     public float DistanceFromGround = 0.5f;
     public float DropRate = 0.3f;
@@ -12,27 +13,67 @@ public class CharController : MonoBehaviour {
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    _positionVector = this.transform.position;
+    private LayerMask _terrainLayerMask;
+    private LayerMask _ladderLayerMask;
+    private LayerMask _interactableLayerMask;
+
+    private bool _canClimb = false;
+    private bool _interactabtable = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        _positionVector = this.transform.position;
         _animator = this.GetComponent<Animator>();
         _spriteRenderer = this.GetComponent<SpriteRenderer>();
-	}
-	
-	void FixedUpdate()
-    {
-        _positionVector.y -= DropRate;
-        this.transform.position = _positionVector;
+        _terrainLayerMask = LayerMask.GetMask("Terrain");
+        _ladderLayerMask = LayerMask.GetMask("Ladder");
+        _interactableLayerMask = LayerMask.GetMask("Interactable");
+    }
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.up, -Vector2.up);
-        if (hit.collider != null) {
-            float distance = Mathf.Abs(hit.point.y - transform.position.y);
-            float heightError = DistanceFromGround - distance;
-            _positionVector.y += heightError;
+    void FixedUpdate()
+    {
+        if (!_canClimb)
+        {
+            _positionVector.y -= DropRate;
+            this.transform.position = _positionVector;
+
+            Debug.Log(transform.position);
+
+            RaycastHit2D terrainHit = Physics2D.Raycast(transform.position + Vector3.up, -Vector2.up, 10, _terrainLayerMask.value);
+            if (terrainHit.collider != null)
+            {
+
+                float distance = Mathf.Abs(terrainHit.point.y - transform.position.y);
+                float heightError = DistanceFromGround - distance;
+                _positionVector.y += heightError;
+            }
         }
 
-        Debug.DrawRay(transform.position + Vector3.up, -Vector2.up * DistanceFromGround, Color.green);
+        RaycastHit2D ladderHit = Physics2D.Raycast(transform.position, Vector2.up, 0.1f, _ladderLayerMask);
+        if (ladderHit.collider != null)
+        {
+            _canClimb = true;
+        }
+        else
+        {
+            _canClimb = false;
+        }
+
+        if (_canClimb)
+        {
+            _positionVector.y += Input.GetAxis("Vertical");
+        }
+
+        RaycastHit2D interactableHit = Physics2D.Raycast(transform.position, Vector2.down, 3, _interactableLayerMask);
+        if (interactableHit.collider != null)
+        {
+            _interactabtable = true;
+        }
+        else 
+        {
+            _interactabtable = false;
+        }
 
         float inputX = Input.GetAxis("Horizontal");
 
@@ -50,7 +91,6 @@ public class CharController : MonoBehaviour {
             _spriteRenderer.flipX = true;
         }
 
-
         if (ismoving && !wasmoving)
         {
             _animator.SetBool("Walking", true);
@@ -60,7 +100,11 @@ public class CharController : MonoBehaviour {
             _animator.SetBool("Walking", false);
         }
 
-
+        if (Input.GetKeyDown(KeyCode.Space) && _interactabtable)
+        {
+            //Trigger interactable script here
+            _animator.Play("Hurt");
+        }
 
         this.transform.position = _positionVector;
     }
