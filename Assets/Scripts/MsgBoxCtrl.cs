55e615da02dd129b0f46a2a861686e483d38d29e
noppelmax <gitkraken@noppelmax.online>
2018-01-27 16:24:19 +0100

    Impl MsgBox

---
+++ Assets/Scripts/MsgBoxCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MsgBoxCtrl : MonoBehaviour {
+
+
+	public GameObject guiGameObj;
+	private UnityEngine.UI.Text guiText;
+	private string msg;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		//guiText.text = "Hallo WElt";
+		guiText = guiGameObj.GetComponent<UnityEngine.UI.Text>();
+		guiText.text = "";
+		print(Time.time);
+		yield return new WaitForSecondsRealtime(2);
+
+		print(Time.time);
+     	Destroy(this.gameObject);
+	}
+
+
+	void initialize( string message ){
+		msg = message;
+		guiText.text = msg;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//guiText.text = "Hallo Welt";	
+	}
+}
