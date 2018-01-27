21c1c39685c39929702d7a460099d09bd17f572f
noppelmax <gitkraken@noppelmax.online>
2018-01-27 13:56:15 +0100

    add CamCtrl

---
+++ Assets/Scripts/CamCtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCtrl : MonoBehaviour {
+
+
+	public GameObject player;       //Public variable to store a reference to the player game object
+
+
+	public Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+	private static Vector3 ChangeX(Vector3 v, float x)
+     {
+         return new Vector3(x, v.y, v.z);
+     }
+ 
+     private static Vector3 ChangeY(Vector3 v, float y)
+     {
+         return new Vector3(v.x, y, v.z);
+     }
+ 
+     private static Vector3 ChangeZ(Vector3 v, float z)
+     {
+         return new Vector3(v.x, v.y, z);
+     }
+
+	// Use this for initialization
+	void Start ()
+	{
+			//Calculate and store the offset value by getting the distance between the player's position and camera's position.
+			offset = transform.position - player.transform.position;
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate ()
+	{
+			// Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+			transform.position = ChangeY(transform.position,player.transform.position.y + offset.y);
+	}
+}
