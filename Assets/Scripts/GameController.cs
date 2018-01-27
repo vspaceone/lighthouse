3cf9fc6b78e372b9b3c157620f3d7eba344d4a87
Alexander Scheurer <mail@aspepex.net>
2018-01-27 19:53:02 +0100

    Audio Stuff

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,8 +7,6 @@ public class GameController : MonoBehaviour
 
     public GameObject msgBoxPrefab;
 
-    public GameObject AudioPrefab;
-
     public void Action(string Action, GameObject GO)
     {
         //biggest f-ing switch monster ever
@@ -17,21 +15,21 @@ public class GameController : MonoBehaviour
         switch (Action)
         {
             case "schlafen":
-            {
-                GameObject gObj = Instantiate(msgBoxPrefab);
-                MsgBoxCtrl mBoxCtrl = gObj.GetComponent<MsgBoxCtrl>();
-                mBoxCtrl.Initialize("Tzzzzzz....");
-                break;
-            }
+                {
+                    GameObject gObj = Instantiate(msgBoxPrefab);
+                    MsgBoxCtrl mBoxCtrl = gObj.GetComponent<MsgBoxCtrl>();
+                    mBoxCtrl.Initialize("Tzzzzzz....");
+                    break;
+                }
             case "dose_holen":
-            {
-                GameObject gObj = Instantiate(msgBoxPrefab);
-                MsgBoxCtrl mBoxCtrl = gObj.GetComponent<MsgBoxCtrl>();
-                mBoxCtrl.Initialize("Mhmmm Ravioli. Lecker.");
-                break;
-            }
+                {
+                    GameObject gObj = Instantiate(msgBoxPrefab);
+                    MsgBoxCtrl mBoxCtrl = gObj.GetComponent<MsgBoxCtrl>();
+                    mBoxCtrl.Initialize("Mhmmm Ravioli. Lecker.");
+                    break;
+                }
             case "Start":
-                Day1AlarmClock();
+                Day1Ocean();
                 break;
             default:
                 Debug.Log(GO.name + " sent action " + Action);
@@ -40,14 +38,15 @@ public class GameController : MonoBehaviour
         }
     }
 
-    void PlayAudio(string name, bool loop=false)
+    void PlayAudio(string name, bool loop = false)
     {
         AudioClip audioClip = Resources.Load("Audio/" + name) as AudioClip;
-        GameObject go = Instantiate(new GameObject("Audio-" + name)) as GameObject;
+        GameObject go = new GameObject("Audio-" + name);
         if (!loop) Destroy(go, audioClip.length);
 
         go.transform.parent = Camera.main.transform;
         go.transform.localPosition = Vector3.zero;
+        go.tag = "Audio";
 
         AudioSource audioSource = go.AddComponent<AudioSource>();
         audioSource.clip = audioClip;
@@ -57,14 +56,45 @@ public class GameController : MonoBehaviour
         audioSource.Play();
     }
 
+    IEnumerator FadeOutSound(GameObject go, float duration = 2f)
+    {
+        AudioSource audioSource = go.GetComponent<AudioSource>();
+        float startVol = audioSource.volume;
+        float rate = 1.0f / duration;
+
+        for (float x = 0.0f; x <= 1.0f; x += Time.deltaTime * rate)
+        {
+            audioSource.volume = Mathf.Lerp(startVol, 0, x);
+            yield return null;
+        }
+        Destroy(go);
+    }
+
+
+    void FadeAllAudio(float duration = 2f)
+    {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Audio");
+
+        foreach (var go in gos)
+        {
+            StartCoroutine(FadeOutSound(go, duration));
+        }
+    }
+
     IEnumerator Start()
     {
+        Day1Ocean();
         yield return new WaitForSeconds(10);
-        Action("Start", this.gameObject);
+        Day1AlarmClock();
     }
 
     void Day1AlarmClock()
     {
         PlayAudio("319490__margau__30-seconds-alarm");
+    }
+
+    void Day1Ocean()
+    {
+        PlayAudio("400632__inspectorj__ambience-seaside-waves-close-a");
     }
 }
